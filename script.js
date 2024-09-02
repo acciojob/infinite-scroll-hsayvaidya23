3ba9f23e472f414bbe802d5ecba824999c12769e
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-02 20:57:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+const InfiniteList = document.getElementById('infi-list');
 
+const addListItems = (count = 2) => {
+	for(let i=0; i < count ; i++ ) {
+		const ListItem = document.createElement('li');
+		ListItem.textContent = `Item ${InfiniteList.children.length +  1}`;
+		InfiniteList.appendChild(ListItem);
+	}
+}
+
+addListItems(10);
+
+InfiniteList.addEventListener('scroll', () => {
+	if (InfiniteList.scrollTop + InfiniteList.clientHeight >= InfiniteList.scrollHeight) {
+        addListItems(2);
+    }
+})
